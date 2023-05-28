827e11abcd8396bfda495892808277d0ef1bc1b8
02arslon <arslonruziboyev@gmail.com>
2023-05-29 08:52:41 +0900

    changed

--- src/components/EpPost.jsx
+++ src/components/EpPost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState, useParams, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const EpPost = () => {
   const { empid } = useParams();
@@ -7,7 +8,7 @@ const EpPost = () => {
   const [empdata, empdatachange] = useState({});
 
   useEffect(() => {
-    fetch("http://localhost:8000/employee/" + empid)
+    fetch("http://localhost:7000/employment/" + empid)
       .then((res) => {
         return res.json();
       })
@@ -18,9 +19,22 @@ const EpPost = () => {
         console.log(err.message);
       });
   }, []);
+
   return (
     <div>
-      <h1>The Employee name is :</h1>
+      {empdata && (
+        <h1>
+          <h2>
+            The Employee name is : <b>{empdata.name}</b> ({empdata.id})
+          </h2>
+          <h3>Contact Details</h3>
+          <h5>Email is : {empdata.email}</h5>
+          <h5>Phone is : {empdata.phone}</h5>
+          <Link className="btn btn-danger" to="/">
+            Back to Listing
+          </Link>
+        </h1>
+      )}
     </div>
   );
 };
