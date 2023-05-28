bd0fb105c7e0f59200ed42dcfb6b45495e0a806c
02arslon <arslonruziboyev@gmail.com>
2023-05-29 08:44:30 +0900

    chnaged

--- src/components/EpPost.jsx
+++ src/components/EpPost.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState, useParams, useEffect } from "react";
 
 const EpPost = () => {
-  return <div>Detailss</div>;
+  const { empid } = useParams();
+
+  const [empdata, empdatachange] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:8000/employee/" + empid)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        empdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Employee name is :</h1>
+    </div>
+  );
 };
 
 export default EpPost;
